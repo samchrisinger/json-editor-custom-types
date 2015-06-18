793f2bde2d8fb67750d3c3c40c0e255d8d28afb6
Eric Barbour <emb4gu@virginia.edu>
2015-06-18 11:18:11 -0400

    More extended but ugly commenting

--- run.js
+++ run.js
@@ -1,26 +1,37 @@
 $(document).ready(function() {
+
     // Custom Template Engines
     var onClickComment = function() {
-        console.log(this);
+        //console.log(this);
     };
+
+    var onClickEdit = function() {
+        console.log(this);
+        $("input.comment").toggleClass("comment-edited");
+        var inputAbove = this.previousSibling;
+
+    };
+
     var Comment = JSONEditor.defaults.editors.string.extend({
         register: function() {
             this._super();
             if (!this.input) {
                 return;
             }
+            // base comment attributes, starts as editable
             this.input.onclick = onClickComment;
+            this.input.className = "comment comment-base comment-enabled ";
+
+            // each comment has an edit button placed right after it
+            var editBtn = document.getElementById("edit-btn");
+            var parentDiv = this.input.parentNode;
+            // no insertAfter function so insert it before its sibling
+            parentDiv.insertBefore (editBtn, this.input.nextSibling);
+            editBtn.onclick = onClickEdit;
         }
     });
+
     JSONEditor.defaults.editors.comment = Comment;
-    /*
-    JSONEditor.defaults.resolvers.unshift(function(schema) {
-        if(schema.type === 'comment') {
-            return "comment";
-        }
-    });
-     */
-    /////
 
     var element = document.getElementById('editor_holder');
 
@@ -54,6 +65,7 @@ $(document).ready(function() {
             },
             comments: {
                 type: 'array',
+                title: 'Car Discussion',
                 items: {
                     type: 'comment'
                 }
@@ -64,8 +76,16 @@ $(document).ready(function() {
     var options = {
         schema: schema,
         disable_properties: true,
-        disable_edit_json: true
+        disable_array_reorder: true,
+        disable_edit_json: true,
+        theme: 'bootstrap3'
     };
 
     var editor = new JSONEditor(element, options);
+
+    $('input[type="button"]').click(function() {
+
+        $('.comment').prop('disabled', function(i,oldVal) { return !oldVal; });
+
+    });
 });
